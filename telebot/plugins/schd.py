55f62ee6a95420515640f1bdc0d56498c48feb18
ankitkumarbh <ankitkumarbh@gmail.com>
2021-10-20 13:16:44 +0530

    Update schd.py

--- telebot/plugins/schd.py
+++ telebot/plugins/schd.py
@@ -1,32 +1,46 @@
-"""Schedule Plugin for @UniBorg
-Syntax: .schd <time_in_seconds> ;=; <message to send>"""
 import asyncio
+from telethon.tl.functions.channels import EditAdminRequest
+from telethon.tl.functions.contacts import BlockRequest, UnblockRequest
+from telethon.tl.types import ChatAdminRights
+from telethon.errors.rpcerrorlist import ChatSendMediaForbiddenError, PeerIdInvalidError 
 
-from telebot import CMD_HELP
-from telebot.utils import admin_cmd
 
+from . import *
 
 @telebot.on(admin_cmd(pattern="schd ?(.*)"))
-async def _(event):
-    if event.fwd_from:
-        return
-    input_str = event.pattern_match.group(1)
-    ttl = 0
-    message = "SYNTAX: `.schd <time_in_seconds> = <message to send>`"
-    if input_str:
-        await event.delete()
-        if "=" in input_str:
-            ttl, message = input_str.split("=")
-        elif event.reply_to_msg_id:
-            await event.delete()
-            ttl = int(input_str)
-            message = await event.get_reply_message()
-        await asyncio.sleep(int(ttl))
-        await event.respond(message)
-    else:
-        await event.edit(message)
-
-
-CMD_HELP.update(
-    {"schd": ".schd <time in sec> = <mssg to send>\nUse - Send a scheduled message."}
-)
+@telebot.on(sudo_cmd(pattern="schd ?(.*)", allow_sudo=True))
+async def schd(event):
+    a = event.pattern_match.group(1)
+    b = a.split(" ")
+    wwait = b[0]
+    times = int(b[1])
+    idds = b[2]
+    previous_message = await event.get_reply_message()
+    if previous_message:
+        previous_message = await event.get_reply_message()
+        idds = previous_message.id
+    if idds:
+        idds = int(b[2])
+    kk = await event.reply("`Schedule Broadcasting Msg...`")
+    er = 0
+    done = 0
+    count = 0
+    chatidd = await event.get_chat()
+    chatidd = chatidd.id
+    while count != times:
+        count += 1
+        er = 0
+        done = 0
+        await asyncio.sleep(int(wwait))
+        await kk.edit("`Broadcasting...`")
+        msg = await borg.get_messages(chatidd, ids=idds)
+        async for x in event.client.iter_dialogs():
+            if x.is_group:
+                chat = x.id
+                try:
+                    done += 1
+                    await borg.send_message(chat, msg)
+                except BaseException:
+                    er += 1
+        await kk.edit(f"Done in {done} chats, error in {er} chat(s)")
+    await kk.reply("`Schedule Broadcast Finished...`")
